3ac3a656bd3d07c698b8c93b811512138930338e
Joshua Relton <joshuadrelton@gmail.com>
2020-10-06 19:23:19 -0500

    Added additional parsing for common domains

--- domainExpiration/sampleInput.py
+++ domainExpiration/sampleInput.py
@@ -1,7 +1,7 @@
 import sourceFiles
 
 # Create a list of sample domains that we can test
-domainList = ["google.co.uk", "google.com", "yahoo.com", "www.facebook.com", "ddarko.pl", "incorrect.test", "facebook.ca"]
+domainList = ["google.co.uk", "mozilla.org", "php.net", "google.com", "yahoo.com", "www.facebook.com", "ddarko.pl", "incorrect.test", "facebook.ca"]
 
 # Retrieve expiration date for each domain
 for d in domainList:
--- domainExpiration/sourceFiles/runQuery.py
+++ domainExpiration/sourceFiles/runQuery.py
@@ -8,16 +8,24 @@ def findExpiration(d, tld):
 	p = subprocess.Popen(['whois', domain], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 	# Retrieve results and decode the unicode result
-	r = p.communicate()
-	r = r[0].decode()
+	r = p.communicate()[0]
+	r = r.decode()
 	
-
 	# Condense the output string
 	whois_str = r.split("source:       IANA")
+
 	if len(whois_str) == 2:
 		whois_str = whois_str[1]
-	else:
-		return None
 
-	whois_str = r[r.find('Domain Name:'):]
+	# Further parsing for some common domains
+	if tld.lower() == "com":
+		whois_str = whois_str[whois_str.find('Registry Expiry Date:'):whois_str.find('Registrar:')]
+	if tld.lower() == "org":
+		whois_str = whois_str[whois_str.find('Registrar Registration Expiration Date: '):whois_str.find('Registrar: MarkMonitor, Inc')]
+	if tld.lower() == "net":
+		whois_str = whois_str[whois_str.find('Registry Expiry Date:'):whois_str.find('Registrar:')]
+
+
+
+
 	return whois_str
